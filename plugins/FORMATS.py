178006057e5e0e641cb22ae9d8ba49a62e1be365
Project999x <105520683+Project999x@users.noreply.github.com>
2025-04-04 18:47:34 +0530

    Update FORMATS.py

--- plugins/FORMATS.py
+++ plugins/FORMATS.py
@@ -2,8 +2,8 @@
 
 #text formatting....
 
-on_pic = "https://graph.org/file/4ce3606b5e8d122d12a71-2dd4ea284b077812c7.jpg"
-off_pic = "https://graph.org/file/4ce3606b5e8d122d12a71-2dd4ea284b077812c7.jpg"
+on_pic = "https://graph.org/file/831a084c1e331e34e7616-e9f066b3ac85b01253.jpg"
+off_pic = "https://graph.org/file/831a084c1e331e34e7616-e9f066b3ac85b01253.jpg"
 files_cmd_pic = "https://envs.sh/Nyc.jpg"
 autodel_cmd_pic = "https://envs.sh/NyZ.jpg"
 
